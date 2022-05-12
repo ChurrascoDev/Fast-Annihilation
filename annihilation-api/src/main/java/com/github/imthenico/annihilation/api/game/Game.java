098070e20a7b44faa06b4e296cf9b06846cd2225
ImTheNico_ <theunclebartodev@gmail.com>
2022-05-12 14:57:03 -0300

    make Game interface and move internal implementations to plugin module

--- annihilation-api/src/main/java/com/github/imthenico/annihilation/api/game/Game.java
+++ annihilation-api/src/main/java/com/github/imthenico/annihilation/api/game/Game.java
@@ -1,128 +1,26 @@
 package com.github.imthenico.annihilation.api.game;
 
 import com.github.imthenico.annihilation.api.match.Match;
-import com.github.imthenico.annihilation.api.match.MatchFactory;
 import com.github.imthenico.annihilation.api.match.authorization.AuthorizationResult;
 import com.github.imthenico.annihilation.api.match.authorization.MatchAuthorizer;
-import com.github.imthenico.annihilation.api.match.expansion.MatchExpansion;
-import com.github.imthenico.annihilation.api.model.map.data.MatchMapData;
-import com.github.imthenico.annihilation.api.strategy.MatchMapModelProvider;
-import com.github.imthenico.annihilation.api.util.VoteCounter;
-import com.github.imthenico.annihilation.api.validator.MapCandidateValidator;
-import com.github.imthenico.gmlib.MapModel;
 
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
+public interface Game {
+    GameState calculateState();
 
-public class Game {
+    AuthorizationResult startMatch();
 
-    private final GameRoom owner;
-    private final Options options;
-    private final Rules rules;
-    private final MatchFactory matchFactory;
-    private final MatchExpansion matchExpansion;
-    private final PreMatchStage preparationStage;
-    private final MatchAuthorizer matchAuthorizer;
-    private final MatchMapModelProvider matchMapModelProvider;
+    Match runningMatch();
 
-    private Match match;
+    MatchAuthorizer getMatchAuthorizer();
 
-    public Game(
-            GameRoom owner,
-            MatchFactory matchFactory,
-            MatchExpansion matchExpansion,
-            MatchAuthorizer matchAuthorizer,
-            MatchMapModelProvider matchMapModelProvider,
-            MapCandidateValidator mapCandidateValidator,
-            int timeToStart
-    ) {
-        this.owner = owner;
-        this.options = owner.getOptions().copy();
-        this.rules = owner.getRules().copy();
-        this.matchFactory = matchFactory;
-        this.matchExpansion = matchExpansion;
-        this.matchAuthorizer = matchAuthorizer;
-        this.matchMapModelProvider = matchMapModelProvider;
-        this.preparationStage = new SimplePreMatchStage(timeToStart, this, matchMapModelProvider, mapCandidateValidator);
-    }
+    PreMatchStage getPreparationStage();
 
-    public GameState calculateState() {
-        if (match != null) {
-            if (match.getClosingStage().isRunning()) {
-                return GameState.ENDING;
-            }
+    String getTypeName();
 
-            return GameState.IN_GAME;
-        }
+    Options getOptions();
 
-        if (preparationStage.getCountdownToStart().getElapsedTime() > 0) {
-            return GameState.STARTING;
-        }
+    Rules getRules();
 
-        return GameState.WAITING;
-    }
+    GameRoom room();
 
-    public PreMatchStage getPreparationStage() {
-        return preparationStage;
-    }
-
-    public AuthorizationResult startMatch() {
-        if (match != null) {
-            throw new UnsupportedOperationException("There's already a match running.");
-        }
-
-        AuthorizationResult result = matchAuthorizer.canStart(this);
-
-        if (result.isAuthorized()) {
-            VoteCounter<UUID, String> voteCounter = preparationStage.getVotes();
-
-            String votedMapName = voteCounter.mostVoted();
-
-            if (votedMapName == null && options.isSelectRandomMap()) {
-                List<String> candidates = voteCounter.getCandidates();
-
-                int i = ThreadLocalRandom.current().nextInt(candidates.size() - 1);
-
-                votedMapName = candidates.get(i);
-            } else {
-                throw new RuntimeException("Unable to select a map name");
-            }
-
-            MapModel<? extends MatchMapData> mapModel = matchMapModelProvider.getModel(votedMapName);
-
-            this.match = matchFactory.createMatch(this, matchExpansion, mapModel);
-            match.start();
-        }
-
-        return result;
-    }
-
-    public GameRoom room() {
-        return owner;
-    }
-
-    public Match runningMatch() {
-        return match;
-    }
-
-    public MatchAuthorizer matchAuthorizer() {
-        return matchAuthorizer;
-    }
-
-    /**
-     * This method is used to identify different types of
-     * games with different game functions, characteristics, etc.
-     */
-    public String getTypeName() {
-        return owner.getTypeName();
-    }
-
-    public Options getOptions() {
-        return options;
-    }
-
-    public Rules getRules() {
-        return rules;
-    }
 }
