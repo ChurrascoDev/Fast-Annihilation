36aeab678dd1a1037c86e5467f1093ae31300513
ImTheNico_ <theunclebartodev@gmail.com>
2022-05-13 16:13:32 -0300

    refactor: merged MatchMapModelProvider with MapCandidateValidator

--- annihilation-api/src/main/java/com/github/imthenico/annihilation/api/strategy/MatchMapModelProvider.java
+++ annihilation-api/src/main/java/com/github/imthenico/annihilation/api/strategy/MatchMapModelProvider.java
@@ -1,12 +1,13 @@
 package com.github.imthenico.annihilation.api.strategy;
 
 import com.github.imthenico.annihilation.api.model.map.MatchMapData;
+import com.github.imthenico.annihilation.api.validator.MapCandidateValidator;
 import com.github.imthenico.gmlib.MapModel;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public interface MatchMapModelProvider {
+public interface MatchMapModelProvider extends MapCandidateValidator {
 
     @NotNull MapModel<MatchMapData> getModel(String name);
 
--- fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/strategy/DefaultMatchMapModelProvider.java
+++ fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/strategy/DefaultMatchMapModelProvider.java
@@ -1,8 +1,10 @@
 package com.github.imthenico.fastannihilation.strategy;
 
+import com.github.imthenico.annihilation.api.game.Game;
 import com.github.imthenico.annihilation.api.model.ModelCache;
 import com.github.imthenico.annihilation.api.model.map.MatchMapData;
 import com.github.imthenico.annihilation.api.strategy.MatchMapModelProvider;
+import com.github.imthenico.annihilation.api.validator.MapCandidateValidator;
 import com.github.imthenico.gmlib.MapModel;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultMatchMapModelProvider implements MatchMapModelProvider {
+
+    private final static MapCandidateValidator DELEGATE = MapCandidateValidator.defaultValidator();
 
     private final ModelCache modelCache;
 
@@ -31,5 +35,10 @@ public class DefaultMatchMapModelProvider implements MatchMapModelProvider {
         }
 
         return modelNames;
+    }
+
+    @Override
+    public boolean isValid(String mapName, Game game) {
+        return DELEGATE.isValid(mapName, game);
     }
 }
