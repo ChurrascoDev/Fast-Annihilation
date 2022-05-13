df2bf930474f679ca6548019afb4fb93feb649f9
ImTheNico_ <theunclebartodev@gmail.com>
2022-05-13 15:08:24 -0300

    refactor: removed model installation when displays a board

--- fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/service/ScoreboardServiceImpl.java
+++ fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/service/ScoreboardServiceImpl.java
@@ -1,11 +1,6 @@
 package com.github.imthenico.fastannihilation.service;
 
 import com.github.imthenico.annihilation.api.board.CompositeFrameInterceptor;
-import com.github.imthenico.annihilation.api.game.Game;
-import com.github.imthenico.annihilation.api.game.GameRoom;
-import com.github.imthenico.annihilation.api.game.GameState;
-import com.github.imthenico.annihilation.api.player.AnniPlayer;
-import com.github.imthenico.annihilation.api.player.PlayerRegistry;
 import com.github.imthenico.annihilation.api.scoreboard.UnDeletableComplexBoard;
 import com.github.imthenico.annihilation.api.service.ScoreboardService;
 import com.github.imthenico.fastannihilation.scoreboard.PAPIInterceptor;
@@ -18,6 +13,7 @@ import net.hexaway.board.abstraction.model.ScoreboardModelBuilder;
 import net.hexaway.board.abstraction.model.ScoreboardPack;
 import org.bukkit.entity.Player;
 import com.github.imthenico.fastannihilation.scoreboard.ColorInterceptor;
+
 public class ScoreboardServiceImpl implements ScoreboardService {
 
     public static final ScoreboardPack LOBBY_BOARD_MODEL;
@@ -32,7 +28,7 @@ public class ScoreboardServiceImpl implements ScoreboardService {
                 .line(13, "&e")
                 .line(12, "&bLobby: %anniplayer_lobby%")
                 .line(11, "&b")
-                .line(10, "%b%server_ip%")
+                .line(10, "&b%server_ip%")
                 .build();
 
         WAITING_GAME_BOARD_MODEL = new ScoreboardModelBuilder()
@@ -47,7 +43,7 @@ public class ScoreboardServiceImpl implements ScoreboardService {
                 .line(7, "&aGreen: %game_team_green_count%")
                 .line(6, "&9Blue: %game_team_blue_count%")
                 .line(5, "&b")
-                .line(4, "%b%server_ip%")
+                .line(4, "&b%server_ip%")
                 .build();
 
         STARTING_GAME_BOARD_MODEL = new ScoreboardModelBuilder()
@@ -57,7 +53,12 @@ public class ScoreboardServiceImpl implements ScoreboardService {
                 .line(12, "&e")
                 .line(11, "Time to start: &a%game_time%")
                 .line(10, "&b")
-                .line(9, "%b%server_ip%")
+                .line(9, "&cRed: %game_team_red_count%")
+                .line(8, "&eYellow: %game_team_yellow_count%")
+                .line(7, "&aGreen: %game_team_green_count%")
+                .line(6, "&9Blue: %game_team_blue_count%")
+                .line(5, "&a")
+                .line(4, "&b%server_ip%")
                 .build();
 
         GAME_BOARD_MODEL = new ScoreboardModelBuilder()
@@ -70,20 +71,17 @@ public class ScoreboardServiceImpl implements ScoreboardService {
                 .line(9, "&cGreen &anexus: %nexus_green_elegant_health%")
                 .line(8, "&cBlue &anexus: %nexus_blue_elegant_health%")
                 .line(7, "&b")
-                .line(6, "Your team: %matchplayer_team%")
                 .line(5, "Current Phase: %match_current_phase%")
                 .line(4, "Next Phase: %match_next_phase_time%")
                 .line(3, "&f")
-                .line(2, "%b%server_ip%")
+                .line(2, "&b%server_ip%")
                 .build();
     }
 
     private final CompositeFrameInterceptor frameInterceptor = new CompositeFrameInterceptor();
     private final ComplexBoardManager boardManager;
-    private final PlayerRegistry playerRegistry;
 
-    public ScoreboardServiceImpl(PlayerRegistry playerRegistry) {
-        this.playerRegistry = playerRegistry;
+    public ScoreboardServiceImpl() {
         this.boardManager = new SimpleComplexBoardManager();
         boardManager.stop();
         frameInterceptor.addInterceptor(new ColorInterceptor());
@@ -105,8 +103,6 @@ public class ScoreboardServiceImpl implements ScoreboardService {
             board = boardManager.createBoard(player, LineAlgorithm.ANY, frameInterceptor);
         }
 
-        ScoreboardPack pack = getPackByState(playerRegistry.getPlayer(player.getUniqueId()));
-        pack.install(board);
         return board;
     }
 
@@ -127,25 +123,4 @@ public class ScoreboardServiceImpl implements ScoreboardService {
         boardManager.stop();
     }
 
-    private ScoreboardPack getPackByState(AnniPlayer player) {
-        GameRoom room = player.getPlayingRoom();
-        if (room != null) {
-            Game game = room.game();
-
-            if (game != null) {
-                GameState state = game.calculateState();
-                switch (state) {
-                    case WAITING:
-                        return WAITING_GAME_BOARD_MODEL;
-                    case STARTING:
-                        return STARTING_GAME_BOARD_MODEL;
-                    case IN_GAME:
-                    case ENDING:
-                        return GAME_BOARD_MODEL;
-                }
-            }
-        }
-
-        return LOBBY_BOARD_MODEL;
-    }
 }
