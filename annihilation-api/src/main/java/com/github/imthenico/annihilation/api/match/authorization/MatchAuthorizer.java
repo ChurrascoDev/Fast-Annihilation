5511c1e5ce421631538394ad15f19a2e935944bb
ImTheNico_ <theunclebartodev@gmail.com>
2022-05-13 15:48:00 -0300

    feat: added method to validate a model usability to use it as a match map

--- annihilation-api/src/main/java/com/github/imthenico/annihilation/api/match/authorization/MatchAuthorizer.java
+++ annihilation-api/src/main/java/com/github/imthenico/annihilation/api/match/authorization/MatchAuthorizer.java
@@ -1,9 +1,13 @@
 package com.github.imthenico.annihilation.api.match.authorization;
 
 import com.github.imthenico.annihilation.api.game.Game;
+import com.github.imthenico.annihilation.api.model.map.MatchMapData;
+import com.github.imthenico.gmlib.MapModel;
 
 public interface MatchAuthorizer {
 
     AuthorizationResult canStart(Game game);
+
+    AuthorizationResult isEligibleForMap(MapModel<? extends MatchMapData> model);
 
 }
--- fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/match/DefaultMatchAuthorizer.java
+++ fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/match/DefaultMatchAuthorizer.java
@@ -4,7 +4,6 @@ import com.github.imthenico.annihilation.api.game.Game;
 import com.github.imthenico.annihilation.api.game.Rules;
 import com.github.imthenico.annihilation.api.match.authorization.AuthorizationResult;
 import com.github.imthenico.annihilation.api.match.authorization.MatchAuthorizer;
-import com.github.imthenico.annihilation.api.model.ModelCache;
 import com.github.imthenico.annihilation.api.model.map.MatchMapData;
 import com.github.imthenico.annihilation.api.util.VoteCounter;
 import com.github.imthenico.gmlib.MapModel;
@@ -16,12 +15,6 @@ import static com.github.imthenico.annihilation.api.util.GameValidation.*;
 
 public class DefaultMatchAuthorizer implements MatchAuthorizer {
 
-    private final ModelCache modelCache;
-
-    public DefaultMatchAuthorizer(ModelCache modelCache) {
-        this.modelCache = modelCache;
-    }
-
     @Override
     public AuthorizationResult canStart(Game game) {
         Rules rules = game.room().getRules();
@@ -29,31 +22,32 @@ public class DefaultMatchAuthorizer implements MatchAuthorizer {
         VoteCounter<UUID, String> playerVotes = game.getPreparationStage().getVotes();
         boolean authorized;
         String messagePath;
-        String reason = null;
+        String reason;
 
-        MapModel<? extends MatchMapData> mostVoted = modelCache.getModel(playerVotes.mostVoted());
+        String candidate = playerVotes.mostVoted();
 
-        if (!hasAvailableMaps(game, modelCache)) {
-            messagePath = "match-cannot-start.no-available-maps";
-            authorized = false;
-            reason = "No available maps";
-        } else if (mostVoted == null && rules.getDefaultMap() == null && !game.getOptions().isSelectRandomMap()) {
-            messagePath = "match-cannot-start.no-map-voted";
+        if (candidate == null && rules.getDefaultMap() == null && !game.getOptions().isSelectRandomMap()) {
+            messagePath = "match-cannot-start-no-map-voted";
             authorized = false;
             reason = "No map voted";
         } else if (!balancedTeams(game)) {
-            messagePath = "match-cannot-start.unbalanced-teams";
+            messagePath = "match-cannot-start-unbalanced-teams";
             authorized = false;
             reason = "Unbalanced Teams";
-        } else if (mostVoted != null && !isDataCorrectlyConfigured(mostVoted.getData())) {
-            messagePath = "match-cannot-start.map-not-configured";
-            authorized = false;
-            reason = "Map is not configured correctly";
         } else {
             messagePath = "match-can-start";
             authorized = true;
+            reason = "passed checks";
         }
 
         return AuthorizationResult.of(authorized, path(messagePath), reason);
+    }
+
+    @Override
+    public AuthorizationResult isEligibleForMap(MapModel<? extends MatchMapData> model) {
+        if (!isDataCorrectlyConfigured(model.getData()))
+            return AuthorizationResult.of(false, path("invalid-map-for-match"), "");
+
+        return AuthorizationResult.of(true, path("valid-map-for-match"), "");
     }
 }
