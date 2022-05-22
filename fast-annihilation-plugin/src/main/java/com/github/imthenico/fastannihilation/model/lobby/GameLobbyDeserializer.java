f3a32991d80f46978234ea19498a2663a87817b1
Nahuel <68520721+ImTheNico@users.noreply.github.com>
2022-05-22 20:46:52 -0300

    refactor: rename imports, replace json-helper references for Gson references

--- fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/model/lobby/GameLobbyDeserializer.java
+++ fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/model/lobby/GameLobbyDeserializer.java
@@ -1,12 +1,20 @@
 package com.github.imthenico.fastannihilation.model.lobby;
 
 import com.github.imthenico.annihilation.api.model.lobby.GameLobbyData;
-import com.github.imthenico.gmlib.DataDeserializer;
-import com.github.imthenico.json.JsonReader;
+import com.github.imthenico.gmlib.handler.DataDeserializer;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class GameLobbyDeserializer implements DataDeserializer<GameLobbyData> {
+
+    private final Gson gson;
+
+    public GameLobbyDeserializer(Gson gson) {
+        this.gson = gson;
+    }
+
     @Override
-    public GameLobbyData deserializeData(JsonReader jsonReader) {
-        return jsonReader.mapTo(GameLobbyData.class);
+    public GameLobbyData deserializeData(JsonObject jsonObject) {
+        return gson.fromJson(jsonObject, GameLobbyData.class);
     }
 }
