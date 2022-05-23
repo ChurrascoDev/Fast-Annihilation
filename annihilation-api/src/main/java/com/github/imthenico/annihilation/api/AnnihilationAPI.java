9b3d28fdb6f03cc26fdd0e53f44f8ca17a889743
Nahuel <68520721+ImTheNico@users.noreply.github.com>
2022-05-23 13:45:04 -0300

    refactor: remove InjectionHandler

--- annihilation-api/src/main/java/com/github/imthenico/annihilation/api/AnnihilationAPI.java
+++ annihilation-api/src/main/java/com/github/imthenico/annihilation/api/AnnihilationAPI.java
@@ -7,11 +7,8 @@ import com.github.imthenico.annihilation.api.service.ModelService;
 import com.github.imthenico.annihilation.api.service.GameService;
 import com.github.imthenico.annihilation.api.service.ScoreboardService;
 import com.github.imthenico.annihilation.api.util.UtilityPack;
-import com.github.imthenico.inject.InjectionHandler;
 
 public interface AnnihilationAPI {
-
-    InjectionHandler INJECTION_HANDLER = InjectionHandler.create();
 
     UtilityPack utilities();
 
