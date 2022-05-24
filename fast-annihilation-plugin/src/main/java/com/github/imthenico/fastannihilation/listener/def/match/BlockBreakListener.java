b7af39d6a76a0be7e13aafd25c30cff5e4f2c74f
Nahuel <68520721+ImTheNico@users.noreply.github.com>
2022-05-24 13:40:42 -0300

    refactor: add some checks to evict invalid nexus hit

--- fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/listener/def/match/BlockBreakListener.java
+++ fast-annihilation-plugin/src/main/java/com/github/imthenico/fastannihilation/listener/def/match/BlockBreakListener.java
@@ -3,14 +3,15 @@ package com.github.imthenico.fastannihilation.listener.def.match;
 import com.github.imthenico.annihilation.api.entity.MatchPlayer;
 import com.github.imthenico.annihilation.api.game.Options;
 import com.github.imthenico.annihilation.api.match.Match;
+import com.github.imthenico.annihilation.api.model.map.MatchMap;
 import com.github.imthenico.annihilation.api.model.map.Nexus;
 import com.github.imthenico.annihilation.api.player.PlayerRegistry;
-import org.bukkit.block.Block;
+import com.github.imthenico.annihilation.api.team.MatchTeam;
+import com.github.imthenico.annihilation.api.team.TeamColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.metadata.MetadataValue;
 
 public class BlockBreakListener implements Listener {
 
@@ -25,18 +26,23 @@ public class BlockBreakListener implements Listener {
         Player player = event.getPlayer();
         MatchPlayer matchPlayer = MatchPlayer.from(playerRegistry.getPlayer(player.getUniqueId()));
 
-        if (matchPlayer == null)
-            return;
+        if (matchPlayer == null) return;
 
-        if (matchPlayer.isDisqualified())
-            return;
+        if (matchPlayer.isDisqualified()) return;
 
         Match match = matchPlayer.getMatch();
-        Nexus nexus = match.getRunningMap()
-                .getNexusByLocation(event.getBlock().getLocation());
+        MatchMap matchMap = match.getRunningMap();
 
-        if (nexus == null)
-            return;
+        Nexus nexus = matchMap.getNexusByLocation(event.getBlock().getLocation());
+        if (nexus == null) return;
+
+        MatchTeam matchTeam = matchPlayer.getTeam();
+        if (matchTeam == null) return;
+
+        TeamColor nexusColor = nexus.color();
+        if (matchTeam.getColor() == nexusColor) return;
+
+        if (nexus.getHealth() <= 0) return;
 
         Options options = match.getGame().getOptions();
         int damage = options.getNexusDamage() * options.getNexusDamageMultiplier();
